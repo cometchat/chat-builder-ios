bd20d0057ad18b5a49024d18a044f56ff3e38256
Dawinder Kaur <dawinder.kaur@cometchat.com>
2025-07-13 03:14:49 +0530

    updated package.swift file

--- Package.swift
+++ Package.swift
@@ -1,22 +1,44 @@
-// swift-tools-version:5.5
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "VCB",
-    platforms: [.iOS(.v13)],
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "VCB",
             targets: ["VCB"]
-        ),
+        )
     ],
     targets: [
+        // Binary targets for .xcframeworks
+        .binaryTarget(
+            name: "CometChatUIKitSwift",
+            path: "Frameworks/CometChatUIKitSwift.xcframework"
+        ),
+        .binaryTarget(
+            name: "CometChatSDK",
+            path: "Frameworks/CometChatSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "CometChatStarscream",
+            path: "Frameworks/CometChatStarscream.xcframework"
+        ),
+
+        // Your VCB target
         .target(
             name: "VCB",
-            dependencies: [],
+            dependencies: [
+                "CometChatUIKitSwift",
+                "CometChatSDK",
+                "CometChatStarscream"
+            ],
             path: "Sources/VCB",
+            resources: [
+                .process("../../Resources/VCBAssets.xcassets")
+            ]
         )
     ]
 )
-
